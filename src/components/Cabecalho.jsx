8b6a2abc93e7181340dee54a585882f90777567e
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-11-30 10:47:43 -0300

    feat: conflito de classes globais

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,9 +1,10 @@
 import Menu from "./Menu.jsx";
+import "./Cabecalho.module.css";
 
 function Cabecalho() {
   return (
     <header>
-      <h1>Olá React! 😎</h1>
+      <h1>Olá <span className="sombra">React!</span> 😎</h1>
       <hr />
       <Menu />
     </header>
