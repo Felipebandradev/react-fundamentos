00ffbfb4bcb706a7fc284008af11dcea048770eb
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-12-01 09:37:54 -0300

    feat: testando

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,32 @@
+/* Menu.jsx */
+
+import styled from "styled-components";
+
+const StyledMenu = styled.nav`
+  height: 50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  a {
+    text-decoration: none;
+    color: white;
+    
+  }
+  a:hover {
+    color: #d4b03b;
+    background-color: #5455aa;
+    
+  }
+`;
+
 function Menu() {
   return (
-    <nav>
+    <StyledMenu>
       <a href="">Home</a>
       <a href="">Produtos</a>
       <a href="">Contato</a>
-    </nav>
+    </StyledMenu>
   );
 }
 
