7145cea7b47b40425bb4b63515201862079e498a
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-12-01 09:16:22 -0300

    feat: Aplicação do Styled Component

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,12 +1,21 @@
+/* Cabecalho.jsx */
 import Menu from "./Menu.jsx";
+
+/* Importação da lib styled-components */
+import styled from "styled-components";
+
+const StyledCabecalho = styled.header`
+  background-color: black;
+  color: white;
+`;
 
 function Cabecalho() {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Olá React! 😎</h1>
       <hr />
       <Menu />
-    </header>
+    </StyledCabecalho>
   );
 }
 
