0c35dd4d6b4a51cf7a6eeeb983ab901be472aef0
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-12-05 11:53:16 -0300

    Resolução sem dessestruturação

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -2,8 +2,8 @@ import { StyledArtigo } from "../styles/styles.js";
 import { DiCode } from "react-icons/di";
 
 /* Definindo props para o componente */
-function Artigo({ titulo, categoria, preco }) {
-  const precoFormatado = preco.toLocaleString("pt-br", {
+function Artigo( props ) {
+  const precoFormatado = props.preco.toLocaleString("pt-br", {
     style: "currency",
     currency: "BRL",
     minimumFractionDigits: 2,
@@ -12,10 +12,10 @@ function Artigo({ titulo, categoria, preco }) {
   return (
     <StyledArtigo>
       <h3>
-        <DiCode /> {categoria}
+        <DiCode /> {props.categoria}
       </h3>
       <p>
-        <b>curso:</b> {titulo}
+        <b>curso:</b> {props.titulo}
       </p>
       <p>
         <b>preço:</b> {precoFormatado}
