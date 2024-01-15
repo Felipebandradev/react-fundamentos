c7acf14c89768e1c1736ad5fc2d22ea5dfed29d3
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-15 11:19:55 -0300

    Gerenciando efeito colateral     do componente para o gerenciamento dos dados da API

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,23 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Produtos(){
 
     /* O state "produtos" é iniciado como um array vazio
      posteriormente (após p carregamento da API),
     ele  será prenchido com os objetos/produtos*/
-   const [produtos, setProdutos] = useState([]);
+    const [produtos, setProdutos] = useState([]);
 
-    const carregarDados = async () =>{
-        try {
-          const resposta = await  fetch(`https://fakestoreapi.com/products`);
-          const dados = await resposta.json();
-          console.log(dados);
-          setProdutos(dados);
-
-        } catch (error){
-            console.error("Houve um erro: "+error)
+    /* Gerenciando efeito colateral
+    do componente para o gerenciamento dos dados da API  */
+    useEffect(() =>{
+        const carregarDados = async () =>{
+            try {
+              const resposta = await  fetch(`https://fakestoreapi.com/products`);
+              const dados = await resposta.json();
+              console.log(dados);
+              setProdutos(dados);
+    
+    
+            } catch (error){
+                console.error("Houve um erro: "+error)
+            }
         }
-    }
+
+        carregarDados();
+    }, [] );
+
+    
 
 
 
