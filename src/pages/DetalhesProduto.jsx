de040ff61e45ea3183f6e19a0610c8e6ee42dd5d
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-16 10:27:40 -0300

    pagina carregando o conteúdo/produto unico

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,23 +1,55 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Imgloading from "../assets/loading.svg";
 
 function DetalhesProduto() {
   /* Os parametros tem que ter o mesmo nome */
   const { id } = useParams();
- 
+
+  const [produto, setProduto] = useState({});
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const carregarDados = async () => {
+      try {
+        const resposta = await fetch(`https://fakestoreapi.com/products/${id}`);
+        const dados = await resposta.json();
+        setProduto(dados);
+        setLoading(false);
+      } catch (error) {
+        console.error("Erro ao Carregar produto: " + error);
+      }
+    };
+    carregarDados();
+  }, [id]);
 
   return (
     <article>
-      <h2>Título...</h2>
-      <p>
-        <b>Categoria:</b> categoria...
-      </p>
-      <p>
-        <b>Preco:</b> preco...
-      </p>
-      <p>Descrição...</p>
-      <p>
-        <img src="" alt="produto..." />
-      </p>
+      <h2 style={{ textAlign: "center" }}>Produto</h2>
+      {loading ? (
+        <p style={{ textAlign: "center" }}>
+          <img src={Imgloading} alt="carregando..." />
+        </p>
+      ) : (
+        <div className="produto">
+          <p style={{ textAlign: "center" }}>
+            <img
+              style={{ maxWidth: "350px", margin: "0.5rem auto" }}
+              src={produto.image}
+              alt={produto.title}
+            />
+          </p>
+          <h3>{produto.title}</h3>
+          <p>
+            <b>Categoria:</b> {produto.category}
+          </p>
+          <p>
+            <b>Preço:</b> {produto.price}
+          </p>
+          <p>{produto.description}</p>
+        </div>
+      )}
     </article>
   );
 }
